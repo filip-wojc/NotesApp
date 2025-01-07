ff9de5da099fd8f333ddc3c25c157c4d5cc34659
DarknessoPirate <darknesso539@gmail.com>
2025-01-07 21:45:08 +0100

    changed note available colors

--- app/src/main/java/com/example/notesapp/domain/models/Note.kt
+++ app/src/main/java/com/example/notesapp/domain/models/Note.kt
@@ -3,8 +3,18 @@ package com.example.notesapp.domain.models
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.example.notesapp.ui.theme.Blue
-import com.example.notesapp.ui.theme.Green
+import com.example.notesapp.ui.theme.Black
+
+import com.example.notesapp.ui.theme.BlueViolet
+import com.example.notesapp.ui.theme.DarkBlue
+import com.example.notesapp.ui.theme.DarkGreen
+import com.example.notesapp.ui.theme.DarkOrange
+import com.example.notesapp.ui.theme.DarkRed
+import com.example.notesapp.ui.theme.DarkViolet
+import com.example.notesapp.ui.theme.LightBlue
+import com.example.notesapp.ui.theme.LightGreen
+import com.example.notesapp.ui.theme.MediumBlue
+import com.example.notesapp.ui.theme.Orange
 import com.example.notesapp.ui.theme.Red
 import com.example.notesapp.ui.theme.Violet
 import com.example.notesapp.ui.theme.Yellow
@@ -36,6 +46,7 @@ data class Note (
     val noteId: Int
 ) {
     companion object {
-        val colors = listOf(Red, Yellow, Green, Blue, Violet)
+        val colors = listOf(Yellow, Orange, DarkOrange, Red, DarkRed, LightGreen, DarkGreen,
+            LightBlue, BlueViolet, Violet, MediumBlue, DarkBlue, DarkViolet, Black)
     }
 }
--- app/src/main/java/com/example/notesapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/notesapp/ui/theme/Color.kt
@@ -4,18 +4,40 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
 val Pink40 = Color(0xFF7D5260)
 
-val Red = Color(0xFFFA8080)
-val Blue = Color(0xFF7591E7)
-val Yellow = Color(0xFFEDF392)
-val Violet = Color(0xFFD1AEF3)
-val Green = Color(0xFFBDEFA0)
+
+
+val Yellow = Color(0xFFffffba)
+val Orange = Color(0xFFffdfba)
+val DarkOrange = Color(0xFFFEBE8C)
+val Red = Color(0xFFffb3ba)
+val DarkRed = Color(0xFFC96868)
+
+val LightGreen = Color(0xFFbaffc9)
+val DarkGreen = Color(0xFF68c4af)
+
+val LightBlue = Color(0xFFbed4f0)
+val MediumBlue = Color(0xFF78DBF9)
+val DarkBlue = Color(0xFF4867C7)
+
+val BlueViolet = Color(0xFFc9c9ff)
+val Violet = Color(0xFFC188EF)
+val DarkViolet = Color(0xFF4b2e83)
+
+val Black = Color(0xFF2c3539)
 
 val Background = Color(0xFFe2eafc)
 val LightYellow = Color(0xFFF0F5C9)
+
+
+
+
+
+
+
+
 
