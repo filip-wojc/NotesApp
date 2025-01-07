7a132dd3c3e29152cd6b8b8f2be63277951529a0
filip-wojc <filipwojcik203@gmail.com>
2025-01-07 12:32:41 +0100

    filter by categories

--- app/src/main/java/com/example/notesapp/domain/use_cases/categories/GetAllCategoriesUseCase.kt
+++ app/src/main/java/com/example/notesapp/domain/use_cases/categories/GetAllCategoriesUseCase.kt
@@ -1,11 +1,13 @@
 package com.example.notesapp.domain.use_cases.categories
 
+import com.example.notesapp.domain.models.Category
 import com.example.notesapp.domain.repositories.CategoryRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetAllCategoriesUseCase (
     private val repository: CategoryRepository
-) {
-    operator fun invoke() {
-        repository.getAllCategories()
+)  {
+    operator fun invoke(): Flow<List<Category>> {
+        return repository.getAllCategories()
     }
 }
