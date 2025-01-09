37f25e1cdaa2e39ecda3120fd65e9c938fc91b90
DarknessoPirate <darknesso539@gmail.com>
2025-01-09 16:31:06 +0100

    fixed app crash when adding notes

--- app/src/main/java/com/example/notesapp/presentation/CreateNote/CreateNoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/presentation/CreateNote/CreateNoteViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.notesapp.presentation.CreateNote
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -13,6 +14,7 @@ import com.example.notesapp.domain.models.Note
 import com.example.notesapp.domain.models.Priority
 import com.example.notesapp.domain.use_cases.categories.CategoryUseCases
 import com.example.notesapp.domain.use_cases.notes.NoteUseCases
+import com.example.notesapp.domain.use_cases.priorities.PriorityUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -22,7 +24,12 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class CreateNoteViewModel @Inject constructor(private val _noteUseCases: NoteUseCases, private val _categoryUseCases: CategoryUseCases)  : ViewModel() {
+class CreateNoteViewModel @Inject constructor(
+    private val _noteUseCases: NoteUseCases,
+    private val _categoryUseCases: CategoryUseCases,
+    private val _priorityUseCases: PriorityUseCases
+): ViewModel() {
+
     private val _title = MutableStateFlow("")
     val title : StateFlow<String> = _title
 
@@ -63,35 +70,58 @@ class CreateNoteViewModel @Inject constructor(private val _noteUseCases: NoteUse
 
     fun updateSelectedPriority(priority: Priority) {
         selectedPriority = priority
+
     }
 
     fun updateSelectedCategory(category: Category) {
         selectedCategory = category
+        Log.d("SelectedCategoryChangedTo", "ChangedToName:${selectedCategory.name},Id:${selectedCategory.categoryId}")
     }
 
     fun fetchCategories() {
         viewModelScope.launch {
-            _categoryUseCases.getAllCategories().collect{
-                categoriesList ->
-                _categories.clear()
-                _categories.addAll(categoriesList)
+            _categoryUseCases.getAllCategories().collect{ fetchedCategories ->
+                if(fetchedCategories.isEmpty()){
+                    val c1 = Category("General",1)
+                    val c2 = Category("Important", 2)
+                    val c3 = Category("Other", 3)
+                    _categoryUseCases.upsertCategory(c1)
+                    _categoryUseCases.upsertCategory(c2)
+                    _categoryUseCases.upsertCategory(c3)
+                    _categories.clear()
+                    _categories.addAll(listOf(c1,c2,c3))
+                }
+                else{
+                    _categories.clear()
+                    _categories.addAll(fetchedCategories)
+                }
             }
-
         }
     }
 
     fun fetchPriorities()
     {
-        val p1 = Priority("Low", 1)
-        val p2 = Priority("Medium", 2)
-        val p3 = Priority("High", 3)
-
-        val temp = mutableListOf(p1,p2,p3)
-
-        _priorities.clear()
-        _priorities.addAll(temp)
-
-
+        viewModelScope.launch {
+            _priorityUseCases.getAllPriorities().collect{ fetchedPriorities ->
+                // Initialize priorities if db empty
+                if(fetchedPriorities.isEmpty()){
+                    val p1 = Priority("Low", 1)
+                    val p2 = Priority("Medium", 2)
+                    val p3 = Priority("High", 3)
+                    _priorityUseCases.upsertPriority(p1)
+                    _priorityUseCases.upsertPriority(p2)
+                    _priorityUseCases.upsertPriority(p3)
+                    _priorities.clear()
+                    // update vm priorities list with newly created priorities
+                    _priorities.addAll(listOf(p1,p2,p3))
+                }
+                // Otherwise save priorities if any present
+                else{
+                    _priorities.clear()
+                    _priorities.addAll(fetchedPriorities)
+                }
+            }
+        }
     }
 
     fun addCategory(newCategory: Category) {
@@ -102,6 +132,7 @@ class CreateNoteViewModel @Inject constructor(private val _noteUseCases: NoteUse
     }
 
     fun saveNote(){
+        Log.d("SelectedCategorySave" ,"BeforeSaveSelectedName:${selectedCategory.name},BeforeSaveSelectedId:${selectedCategory.categoryId}")
         val note = Note(
             _title.value,
             _description.value,
